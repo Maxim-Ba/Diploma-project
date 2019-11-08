ac9cc9e0109b0c891704938bb1b7bb3a0b2fd589
Maxim Balashov <79164211428@yandex.ru>
2019-11-08 10:19:36 +0300

    99.5%

--- webpack.config.js
+++ webpack.config.js
@@ -52,9 +52,11 @@ module.exports = {
                 test: /\.css$/i,
                 use: [
                     
-                    (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
-                    'css-loader', 
-                    'postcss-loader',
+                    {loader:(isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                    
+                    },
+                    {loader:'css-loader'}, 
+                    {loader:'postcss-loader'},
                 ]
             },
             {
