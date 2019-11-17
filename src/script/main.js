a72ea8e99034cd5bc4473b4514bc01569566e150
Maxim Balashov <79164211428@yandex.ru>
2019-11-17 13:44:07 +0300

    validation form

--- src/script/main.js
+++ src/script/main.js
@@ -3,6 +3,7 @@ import NewsApi from './modules/ApiNews';
 import Card from './modules/Card';
 import HideOrVisualBlock from './modules/MainBlockModule';
 import removeOldCards from './functions/removeOldCards';
+import Validate from './modules/Validate';
 // ---------Card
 const containerCards = document.querySelector('.results__container_m-r-l-8px');
 // ---------Card
@@ -11,12 +12,16 @@ const queryButton = document.querySelector('.button_blue');
 const loadingBlock = document.querySelector('.loading');
 const nothinFindBlock = document.querySelector('.nothing-find');
 const resultsBlock = document.querySelector('.results');
+const formElement = document.querySelector('form');
+const warningField = document.querySelector('.header__error');
+// ---------ValidateForm
+const validateForm = new Validate(formElement, queryButton, warningField);
+// ---------ValidateForm
 // ---------Api
-
 const news = new NewsApi(queryInput);
-const  showHideLoadingBlock = new HideOrVisualBlock(loadingBlock, 'loading')
-const  showHideNothinFindBlock = new HideOrVisualBlock(nothinFindBlock, 'nothing-find')
-const  showHideResultsBlock = new HideOrVisualBlock(resultsBlock, 'results')
+const  showHideLoadingBlock = new HideOrVisualBlock(loadingBlock, 'loading');
+const  showHideNothinFindBlock = new HideOrVisualBlock(nothinFindBlock, 'nothing-find');
+const  showHideResultsBlock = new HideOrVisualBlock(resultsBlock, 'results');
 queryButton.addEventListener('click', function () {
   showHideLoadingBlock.makeVisible();
   showHideNothinFindBlock.makeHide();
@@ -24,7 +29,7 @@ queryButton.addEventListener('click', function () {
   removeOldCards(containerCards);
   news.getArticlesInformation()
     .then(res => {
-        window.sessionStorage.setItem('request' ,JSON.stringify(res));
+        window.sessionStorage.setItem('request', JSON.stringify(res));
       return showHideFunction();
     })
     .catch((err) => {
@@ -39,7 +44,6 @@ function showHideFunction() {
   
   showHideLoadingBlock.makeHide();
   const informationArticles = JSON.parse(window.sessionStorage.request);
-  console.log(informationArticles);
   if (informationArticles.length === 0) {
     showHideNothinFindBlock.makeVisible();
     return console.log(`Ничего не найдено`);
@@ -50,18 +54,3 @@ function showHideFunction() {
   showHideResultsBlock.makeVisible();
 }
 
-// {
-//   
-//     .then(res => {
-//       console.log(res)
-//       if (res.length == 0) {
-//         return Promise.reject(`Ничего не найдено`);;
-//       }
-//       displayNoneOrBlock(resultsBlock, 'results')
-//       res.forEach(element => {
-//         let card = new Card(element, containerCards);
-//       });
-//     })
-//     .catch(err => console.log(err));
-//   event.preventDefault();
-// }
