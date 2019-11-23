6bb3fee03362f5488c2643b9aaa3e6ddef3389bc
Maxim Balashov <79164211428@yandex.ru>
2019-11-23 23:12:04 +0300

    analytics done

--- src/script/analytics.js
+++ src/script/analytics.js
@@ -1,8 +1,45 @@
 import '../pages/analytics.css';
 import sortByDay from './modules/countAnalytics';
-import nowDay from './functions/nowDay';
+import {nowDay, nowMonth} from './functions/nowDay';
 
 const reqest = JSON.parse(window.localStorage.getItem('request'));
-const numberOfArticles = reqest.length
-sortByDay(reqest);
+const numberOfArticles = reqest.length;
+const analyticsInformation = sortByDay(reqest);
+const mouth = document.querySelector('.diagram__date');
+const newsOfWeek = document.querySelector('#news-of-week');
+const amountOfQuerys = document.querySelector('#amount-of-querys');
+mouth.textContent = 'Дата ' + '(' + nowMonth() + ')';
+newsOfWeek.textContent = numberOfArticles;
 
+
+
+const week = [
+  {day : document.querySelector('#day-1-name')},
+  {day : document.querySelector('#day-2-name')},
+  {day : document.querySelector('#day-3-name')},
+  {day : document.querySelector('#day-4-name')},
+  {day : document.querySelector('#day-5-name')},
+  {day : document.querySelector('#day-6-name')},
+  {day : document.querySelector('#day-7-name')}
+]
+const weekDiagramRightColumn = [
+  {diagram : document.querySelector('#day-1')},
+  {diagram : document.querySelector('#day-2')},
+  {diagram : document.querySelector('#day-3')},
+  {diagram : document.querySelector('#day-4')},
+  {diagram : document.querySelector('#day-5')},
+  {diagram : document.querySelector('#day-6')},
+  {diagram : document.querySelector('#day-7')}
+]
+console.log(analyticsInformation)
+let countOfQuery = 0;
+week.forEach((element, index) => {
+  console.log(analyticsInformation[index].amountArticle)
+  element.day.textContent = analyticsInformation[index].dayIs + ', ' + analyticsInformation[index].dateDay;
+  let amount = analyticsInformation[index].amountKeyWord;
+  weekDiagramRightColumn[index].diagram.textContent = amount;
+  weekDiagramRightColumn[index].diagram.style.background = `linear-gradient(to right, #2F71E5 ${amount}%, #F5F6F7 ${amount}%, #F5F6F7 100%)`
+  countOfQuery += amount;
+  console.log(countOfQuery)
+});
+amountOfQuerys.textContent = countOfQuery;
--- src/script/modules/countAnalytics.js
+++ src/script/modules/countAnalytics.js
@@ -1,18 +1,42 @@
+import { format } from "path";
+
 export default function sortByDay(req) {
   let obj = {
-    0:[],
-    1:[],
-    2:[],
+    0:[],//сегодня
+    1:[],//вчера
+    2:[],//позавчера
     3:[],
     4:[],
     5:[],
     6:[],
   }
-  req.forEach(element => {
-    let dayOfArticle = new Date(element.publishedAt).getDay();
-    obj[dayOfArticle].push(element)
-  });
-  return _countKeyWord(obj);
+  const date = new Date()
+  let day = date.getDate();
+  let i = 0;
+  let prevDay = 0;
+    for (let key in obj) {
+      prevDay = day - i;
+      req.forEach(element => {  
+        if (new Date(element.publishedAt).getDate() === prevDay){
+          obj[key].push(element);
+        }
+      });
+      i=i+1;
+  }  
+  return _countKeyWord(_format(obj));
+}
+function _format(obj) {
+// заполняет пустые свойства обьекта
+  for(let key in obj){
+    if (obj[key].length == 0) {
+      obj[key][0] = {}
+      obj[key][0].description = '';
+      obj[key][0].title = '';
+      let date = new Date(new Date().setDate(new Date().getDate() - key)).toISOString()
+      obj[key][0].publishedAt = date;
+    }
+  }
+  return obj;
 }
 
 function _countKeyWord(obj) {
@@ -21,43 +45,51 @@ function _countKeyWord(obj) {
     0:{
       amountArticle:0,
       amountKeyWord:0,
-      dateDay:0
+      dateDay:0,
+      dayIs: ''
     },
     1:{
       amountArticle:0,
       amountKeyWord:0,
-      dateDay:0
+      dateDay:0,
+      dayIs: ''
     },
     2:{
       amountArticle:0,
       amountKeyWord:0,
-      dateDay:0
+      dateDay:0,
+      dayIs: ''
     },
     3:{
       amountArticle:0,
       amountKeyWord:0,
-      dateDay:0
+      dateDay:0,
+      dayIs: ''
     },
     4:{
       amountArticle:0,
       amountKeyWord:0,
-      dateDay:0
+      dateDay:0,
+      dayIs: ''
     },
     5:{
       amountArticle:0,
       amountKeyWord:0,
-      dateDay:0
+      dateDay:0,
+      dayIs: ''
     },
     6:{
       amountArticle:0,
       amountKeyWord:0,
-      dateDay:0
+      dateDay:0,
+      dayIs: ''
     },
   }
-  for (var key in finalObj) {
+  for (let key in finalObj) {
     finalObj[key].amountArticle = obj[key].length;
     finalObj[key].amountKeyWord +=  _amountKeyWord(window.localStorage.getItem('q'), obj[key])
-    finalObj[key].amountKeyWord =
+    finalObj[key].dateDay =  new Date(obj[key][0].publishedAt).getDate();
+    finalObj[key].dayIs = whatIsDay(new Date(obj[key][0].publishedAt).getDay())
   }
   return finalObj;
 }
@@ -74,4 +106,9 @@ function _amountKeyWord(keyWord, arr) {
     }
   })
     return result;
+}
+
+function whatIsDay(day) {
+  const days = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+  return days[day]
 }
